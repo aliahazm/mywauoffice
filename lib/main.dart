1953e35f004041a88179d2d1a865b0aff410a248
Aliah Azman <aliahazm99@gmail.com>
2022-03-14 11:20:31 +0800

    pubspec updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mywauoffice/Dashboard.dart';
 import 'package:mywauoffice/SignInScreen.dart';
 
 void main() {
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const SignInScreen(),
+      home: const Dashboard(),
     );
   }
 }
